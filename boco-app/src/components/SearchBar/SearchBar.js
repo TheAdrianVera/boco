21f7edf886bcf20162838b448925c4b82c436a28
Juan Zamudio <jzamudio@pandora.com>
2017-07-15 21:55:50 -0700

    Editing search bar

--- boco-app/src/components/SearchBar/SearchBar.js
+++ boco-app/src/components/SearchBar/SearchBar.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import SearchBarField from 'material-ui-search-bar'
+import SearchBarField from 'material-ui-search-bar';
+import AutoComplete from 'react-autocomplete';
 
 class SearchBar extends React.Component {
     render () {
         return (
             <div>
                 <SearchBarField
-                onChange={() => console.log('onChange')}
+                dataSource= {[
+                    
+                ]}
+                onChange={() => console.log('Good!')}
                 onRequestSearch={() => console.log('onRequestSearch')}
                 hintText="What can we help you with?"
                 style={{
