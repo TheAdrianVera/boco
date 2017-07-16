ee836defe32e59f7d9de0d41d24751ecdc6ffc23
TheAdrianVera <agvm94@gmail.com>
2017-07-16 02:32:37 -0700

    backend stuff

--- boco-app/src/index.js
+++ boco-app/src/index.js
@@ -1,9 +1,8 @@
-'use strict'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 // import registerServiceWorker from './registerServiceWorker';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
